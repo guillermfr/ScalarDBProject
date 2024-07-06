af29e6b2eb37d3b21681eb46b9e14224a0637d18
guillermfr <guillerm.fya@orange.fr>
2024-07-06 15:44:51 +0900

    Rename class

--- src/main/java/com/cytech/marketplace/dao/ArticlesDAO.java
+++ src/main/java/com/cytech/marketplace/dao/ArticlesDAO.java
@@ -1,85 +1,109 @@
 package com.cytech.marketplace.dao;
 
-import com.cytech.marketplace.entity.Articles;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+import com.scalar.db.api.*;
+import com.scalar.db.io.Key;
+import com.scalar.db.service.TransactionFactory;
 
-import java.math.BigInteger;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.Properties;
 
 public class ArticlesDAO {
-    public static void addArticle(Articles article) {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
+    private final DistributedTransactionManager manager;
 
-        transaction.begin();
-        em.persist(article);
-        transaction.commit();
-        em.close();
+    public ArticlesDAO() throws IOException {
+        Properties loadProperties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("scalardb.properties")) {
+            loadProperties.load(input);
+        } catch (IOException e) {
+            throw e;
+        }
+
+        TransactionFactory transactionFactory = TransactionFactory.create(loadProperties);
+        manager = transactionFactory.getTransactionManager();
     }
 
-    public static void updateArticle(Articles article) {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-
-        transaction.begin();
-        em.merge(article);
-        transaction.commit();
-        em.close();
+    public void close() {
+        manager.close();
     }
 
-    public static void deleteArticle(Articles article) {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-
-        transaction.begin();
-        em.remove(em.contains(article) ? article : em.merge(article));
-        transaction.commit();
-        em.close();
+    public void loadInitialData() throws Exception {
+        DistributedTransaction transaction = null;
+        try {
+            transaction = manager.start();
+            loadArticleIfNotExists(transaction, 1, "Article 1", 10.0f, 100, "image1");
+            loadArticleIfNotExists(transaction, 2, "Article 2", 20.0f, 200, "image2");
+            loadArticleIfNotExists(transaction, 3, "Article 3", 30.0f, 300, "image3");
+            loadArticleIfNotExists(transaction, 4, "Article 4", 40.0f, 400, "image4");
+            loadArticleIfNotExists(transaction, 5, "Article 5", 50.0f, 500, "image5");
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.abort();
+            }
+            throw e;
+        }
     }
 
-    public static void deleteArticle(UUID uuid) {
-        deleteArticle(getArticle(uuid));
+    private void loadArticleIfNotExists(
+            DistributedTransaction transaction,
+            int id,
+            String name,
+            float price,
+            int stock,
+            String image)
+            throws Exception {
+        Optional<Result> article =
+                transaction.get(
+                        Get.newBuilder()
+                                .namespace("marketplace")
+                                .table("articles")
+                                .partitionKey(Key.ofInt("id", id))
+                                .build());
+        if (!article.isPresent()) {
+            transaction.put(
+                    Put.newBuilder()
+                            .namespace("marketplace")
+                            .table("articles")
+                            .partitionKey(Key.ofInt("id", id))
+                            .textValue("name", name)
+                            .floatValue("price", price)
+                            .intValue("stock", stock)
+                            .textValue("image", image)
+                            .build());
+        }
     }
 
-    public static void deleteArticle(String name) {
-        deleteArticle(getArticle(name));
-    }
+    public String getArticleInfo(int id) throws Exception {
+        DistributedTransaction transaction = null;
+        try {
+            transaction = manager.start();
+            Optional<Result> article =
+                    transaction.get(
+                            Get.newBuilder()
+                                    .namespace("marketplace")
+                                    .table("articles")
+                                    .partitionKey(Key.ofInt("id", id))
+                                    .build());
+            if (!article.isPresent()) {
+                throw new Exception("Article not found");
+            }
 
-    public static Articles getArticle(UUID uuid) {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        Articles article = em.find(Articles.class, uuid);
-        em.close();
-        return article;
-    }
+            transaction.commit();
 
-    public static Articles getArticle(String name) {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        Articles article = em.createQuery("FROM Articles WHERE name = :name", Articles.class).setParameter("name", name).getSingleResult();
-        em.close();
-        return article;
-    }
-
-    public static List<Articles> getArticles() {
-        EntityManager em = PersistenceUtil.getEmf().createEntityManager();
-        List<Articles> articles = em.createQuery("FROM Articles", Articles.class).getResultList();
-        em.close();
-        return articles;
-    }
-
-    public static boolean checkStock(Articles article, int quantity) {
-        Articles updatedArticle = getArticle(article.getId());
-        return updatedArticle.getStock().compareTo(BigInteger.valueOf(quantity)) >= 0;
-    }
-
-    public static boolean checkStock(String name, int quantity) {
-        Articles updatedArticle = getArticle(name);
-        return updatedArticle.getStock().compareTo(BigInteger.valueOf(quantity)) >= 0;
-    }
-
-    public static boolean checkStock(UUID uuid, int quantity) {
-        Articles updatedArticle = getArticle(uuid);
-        return updatedArticle.getStock().compareTo(BigInteger.valueOf(quantity)) >= 0;
+            return String.format(
+                    "Article %d: %s, price: %.2f, stock: %d, image: %s",
+                    id,
+                    article.get().getText("name"),
+                    article.get().getFloat("price"),
+                    article.get().getInt("stock"),
+                    article.get().getText("image"));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.abort();
+            }
+            throw e;
+        }
     }
 }
